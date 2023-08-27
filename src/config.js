f1116444c2bcd5df04cdac2f653135d3f418f515
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-08-27 17:06:46 +0530

    added url statically

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // eslint-disable-next-line no-undef
-export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const backendUrl = import.meta.env.VITE_BACKEND_URL || 'https://b4445wetamil-backend.onrender.com/api';
